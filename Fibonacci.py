d63c86e67f331a8cfbe8d6ebc406bdf79332bf9f
Erkin Bostancı <48890969+erkinbostanci@users.noreply.github.com>
2022-04-11 11:45:07 +0300

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -9,7 +9,7 @@ def gen_fibon(n):
     for i in range(n):
         output.append(a)
         a,b = b,a+b
-    return print(int(output[-1]))
+    return print('Last Fib number: {} Total Fib: {}'.format(int(output[-1]), sum(output)))
 
 seq_size = int(input("How big of a squence dou you want ?: "))
 gen_fibon(seq_size)
